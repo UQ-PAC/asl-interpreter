8bbcfd27370619e3430ff1e7c32583060ce1a497
rina <k@rina.fyi>
2025-02-04 16:01:22 +1000

    install js and add init method to js

--- bin/offline_js.ml
+++ bin/offline_js.ml
@@ -3,6 +3,12 @@ open Asl_utils
 open Asl_ast
 open Js_of_ocaml
 
+let init input out err =
+  Js_of_ocaml.Sys_js.set_channel_flusher stdout out;
+  Js_of_ocaml.Sys_js.set_channel_flusher stderr err;
+  Js_of_ocaml.Sys_js.set_channel_filler stdin input;
+  ()
+
 let dis (opcode: string) : stmt list =
   let op = Z.of_string opcode in
   let bv = Primops.prim_cvt_int_bits (Z.of_int 32) op in
@@ -14,9 +20,15 @@ import("offline_js.bc.js");
 offlineLifter.dis(opcode);
 *)
 
-let () =  Js.export "offlineLifter"
-    begin object%js
-      method dis x = List.map (fun s -> pp_stmt s |> Js.string) (dis (Js.to_string x))
-    end end
+let () = Js.export "aslp_offline"
+  begin object%js
+    method init (out : string -> unit) (err : string -> unit) =
+      Printexc.record_backtrace true;
+      init (fun () -> "") out err
+    method formatException (exn : exn) = Printexc.to_string exn
+    method printException (exn : exn) = output_string stderr (Printexc.to_string exn)
+
+    method dis x = List.map (fun s -> pp_stmt s |> Js.string) (dis (Js.to_string x))
+  end end
 
 
